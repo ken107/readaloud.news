f5f8d9a7efe3d1a7ed9e24ba04208a21e2f89f4c
Hai Phan <hai.phan@gmail.com>
2017-12-28 16:05:51 -0800

    use silence.mp3 for mobile hack

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,59 @@
-var gulp = require("gulp"),
-	newer = require("gulp-newer"),
-	concat = require("gulp-concat"),
-	uglify = require("gulp-uglify"),
-	gulpif = require("gulp-if"),
-	print = require("gulp-print"),
-	argv = require("yargs").argv,
-	del = require("del");
+const {minify} = require("uglify-js");
+const util = require("util");
+const fs = require("fs");
 
-gulp.task("build", function() {
-	var files = [
-		"es6-promise.auto.js",
-		"jquery.js",
-		"bootstrap.js",
-		"databind.js",
-		"components.js",
-	]
-	.map(file => `web/js/${file}`);
+const fsp = {
+	stat: util.promisify(fs.stat),
+	readFile: util.promisify(fs.readFile),
+	writeFile: util.promisify(fs.writeFile),
+};
 
-	gulp.src(files)
-		.pipe(newer("web/js/pack.js"))
-		.pipe(concat("pack.js"))
-		.pipe(gulpif(!argv.d, uglify({compress: {evaluate: false}})))
-		.pipe(print())
-		.pipe(gulp.dest("web/js"));
-});
+
+exports.build = async function() {
+	const jsFiles = ["es6-promise.auto.js", "jquery.js", "bootstrap.js", "databind.js", "components.js"].map(file => `web/js/${file}`);
+	const outFile = "web/js/pack.js";
+	const outFileMin = "web/js/pack.min.js";
+
+	const areNewer = await Promise.all(jsFiles.map(file => isNewer(file, outFile)));
+	if (!areNewer.some(x => x)) {
+		console.log("No changes");
+		return;
+	}
+
+	const str = fs.createWriteStream(outFile);
+	for (const file of jsFiles) await copyStream(fs.createReadStream(file), str);
+	str.end();
+
+	const {error, code} = minify(await fsp.readFile(outFile, "utf8"));
+	if (error) {
+		console.log(error);
+		return;
+	}
+	await fsp.writeFile(outFileMin, code);
+}
+
+
+async function isNewer(srcFile, dstFile) {
+	const srcStat = await fsp.stat(srcFile);
+	try {
+		const dstStat = await fsp.stat(dstFile);
+		return srcStat.mtime > dstStat.mtime;
+	}
+	catch (err) {
+		return true;
+	}
+}
+
+function copyStream(src, dst) {
+	return new Promise(function(fulfill, reject) {
+		src.pipe(dst, {end: false});
+		src.on("error", reject);
+		src.on("end", fulfill);
+	})
+}
+
+if (require.main == module) {
+	const task = process.argv[2];
+	if (task) exports[task]();
+	else console.error("No task specified");
+}
